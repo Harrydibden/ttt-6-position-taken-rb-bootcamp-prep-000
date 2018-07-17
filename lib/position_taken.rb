1dee02dc8c94447a12530a822824c01f6e96bf1b
Harry Dibden <harry-dibden@hotmail.com>
2018-07-17 07:47:40 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -3,7 +3,7 @@ def position_taken?(board, index)
   if board[index] == " " || "" || nil
     return false
   end
-  if board[index] == "X" || "O"
+  if board[index] == "X"
     return true
   end
 end
